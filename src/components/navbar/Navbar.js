26ce241fdf5bf7a718b1dc790c250a22dee9f19d
Siddharth Bankar <siddharthbankar1204@gmail.com>
2024-12-02 09:56:38 +0530

    updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,145 +10,147 @@ import { motion } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
 
 const navVariants = {
-	hidden: {
-		opacity: 0,
-		transition: {
-			delay: 0.5,
-			staggerChildren: 0.2,
-			staggerDirection: -1,
-		},
-	},
-	visible: {
-		opacity: 1,
-		transition: {
-			duration: 0.5,
-			staggerChildren: 0.2,
-			staggerDirection: 1,
-		},
-	},
+  hidden: {
+    opacity: 0,
+    transition: {
+      delay: 0.5,
+      staggerChildren: 0.2,
+      staggerDirection: -1,
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+      staggerDirection: 1,
+    },
+  },
 };
 
 const Navbar = ({ nav, handleNav }) => {
-	return (
-		<AnimatePresence>
-			<motion.nav
-				initial={{ width: "0" }}
-				animate={
-					nav ? { width: "300px" } : { width: "0", transition: { delay: 1 } }
-				}
-				className={nav ? "navbar active" : "navbar"}>
-				<motion.div
-					initial='hidden'
-					whileInView={nav ? "visible" : "hidden"}
-					variants={navVariants}
-					exit='hidden'
-					className='navbar-container'>
-					<div className='top-details'>
-						<div className='img__cover'>
-							<img src={ProfileImg} alt='Main' className='profile-pic-small' />
-						</div>
-						<Link
-							activeClass='active'
-							spy={true}
-							smooth={true}
-							offset={0}
-							duration={500}
-							onClick={handleNav}
-							to='home'
-							className='profile-name'>
-							Siddharth Dadaram Bankar
-						</Link>
-						<NavLinks handleNav={handleNav} />
-					</div>
-					<ul className='mid-details'>
-						<Link
-							activeClass='active'
-							spy={true}
-							smooth={true}
-							offset={0}
-							duration={500}
-							to='home'
-							className='mid-links'>
-							<FaHome className='mid-icon' />
-							<li className='mid-link' onClick={handleNav}>
-								Home
-							</li>
-						</Link>
-						<Link
-							activeClass='active'
-							spy={true}
-							smooth={true}
-							offset={0}
-							duration={500}
-							to='about'
-							className='mid-links'>
-							<FiUser className='mid-icon' />
-							<li className='mid-link' onClick={handleNav}>
-								About
-							</li>
-						</Link>
-						<Link
-							activeClass='active'
-							spy={true}
-							smooth={true}
-							offset={0}
-							duration={500}
-							to='skills'
-							className='mid-links'>
-							<FaLaptop className='mid-icon' />
-							<li className='mid-link' onClick={handleNav}>
-								Skills
-							</li>
-						</Link>
-						<Link
-							activeClass='active'
-							spy={true}
-							smooth={true}
-							offset={0}
-							duration={500}
-							to='services'
-							className='mid-links'>
-							<BiServer className='mid-icon' />
-							<li className='mid-link' onClick={handleNav}>
-								Services
-							</li>
-						</Link>
-						<Link
-							activeClass='active'
-							spy={true}
-							smooth={true}
-							offset={0}
-							duration={500}
-							to='works'
-							className='mid-links'>
-							<BiBookContent className='mid-icon' />
-							<li className='mid-link' onClick={handleNav}>
-								Works
-							</li>
-						</Link>
-						<Link
-							activeClass='active'
-							spy={true}
-							smooth={true}
-							offset={0}
-							duration={500}
-							to='contact'
-							className='mid-links'>
-							<BiEnvelope className='mid-icon' />
-							<li className='mid-link' onClick={handleNav}>
-								Contact
-							</li>
-						</Link>
-					</ul>
-					{/* <div className='copy'>
-						<small className='copyright'>
-							© Copyright ©2024 <br /> All rights reserved |
-						</small>
-					</div> */}
-				</motion.div>
-			</motion.nav>
-		</AnimatePresence>
-	);
+  const handleLinkClick = () => {
+    handleNav(false); 
+  };
+
+  return (
+    <AnimatePresence>
+      <motion.nav
+        initial={{ width: "0" }}
+        animate={
+          nav ? { width: "300px" } : { width: "0", transition: { delay: 1 } }
+        }
+        className={nav ? "navbar active" : "navbar"}
+      >
+        <motion.div
+          initial="hidden"
+          animate={nav ? "visible" : "hidden"}
+          variants={navVariants}
+          exit="hidden"
+          className="navbar-container"
+        >
+          <div className="top-details">
+            <div className="img__cover">
+              <img src={ProfileImg} alt="Main" className="profile-pic-small" />
+            </div>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              onClick={handleLinkClick}
+              to="home"
+              className="profile-name"
+            >
+              Siddharth Dadaram Bankar
+            </Link>
+            <NavLinks handleNav={handleLinkClick} />
+          </div>
+          <ul className="mid-details">
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="home"
+              className="mid-links"
+              onClick={handleLinkClick}
+            >
+              <FaHome className="mid-icon" />
+              <li className="mid-link">Home</li>
+            </Link>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="about"
+              className="mid-links"
+              onClick={handleLinkClick}
+            >
+              <FiUser className="mid-icon" />
+              <li className="mid-link">About</li>
+            </Link>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="skills"
+              className="mid-links"
+              onClick={handleLinkClick}
+            >
+              <FaLaptop className="mid-icon" />
+              <li className="mid-link">Skills</li>
+            </Link>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="services"
+              className="mid-links"
+              onClick={handleLinkClick}
+            >
+              <BiServer className="mid-icon" />
+              <li className="mid-link">Services</li>
+            </Link>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="works"
+              className="mid-links"
+              onClick={handleLinkClick}
+            >
+              <BiBookContent className="mid-icon" />
+              <li className="mid-link">Works</li>
+            </Link>
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="contact"
+              className="mid-links"
+              onClick={handleLinkClick}
+            >
+              <BiEnvelope className="mid-icon" />
+              <li className="mid-link">Contact</li>
+            </Link>
+          </ul>
+        </motion.div>
+      </motion.nav>
+    </AnimatePresence>
+  );
 };
 
 export default Navbar;
